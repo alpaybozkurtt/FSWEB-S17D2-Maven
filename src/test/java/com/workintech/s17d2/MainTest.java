d94741d26514cd35326a03a391b1d3c20d4ca518
alpaybozkurtt <av.alpaybozkurt@gmail.com>
2024-08-20 23:33:17 +0300

    Created & Tested

--- src/test/java/com/workintech/s17d2/MainTest.java
+++ src/test/java/com/workintech/s17d2/MainTest.java
@@ -2,8 +2,10 @@ package com.workintech.s17d2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workintech.s17d2.model.*;
+import com.workintech.s17d2.model.enums.Experience;
 import com.workintech.s17d2.rest.DeveloperController;
 import com.workintech.s17d2.tax.DeveloperTax;
+import com.workintech.s17d2.model.Developer;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,16 +49,16 @@ class MainTest {
         String expectedName = "John Doe";
         double expectedSalary = 1000.0;
         Experience expectedExperience = Experience.JUNIOR;
-        
+
         Developer developer = new Developer(expectedId, expectedName, expectedSalary, expectedExperience);
 
-        
+
         int actualId = developer.getId();
         String actualName = developer.getName();
         double actualSalary = developer.getSalary();
         Experience actualExperience = developer.getExperience();
 
-        
+
         assertEquals(expectedId, actualId, "The ID should match the expected value.");
         assertEquals(expectedName, actualName, "The name should match the expected value.");
         assertEquals(expectedSalary, actualSalary, "The salary should match the expected value.");
@@ -68,7 +70,7 @@ class MainTest {
     @Test
     @DisplayName("Test Experience Enum Values")
     void testEnumValuesExist() {
-        
+
         assertTrue(containsEnumValue("JUNIOR"), "JUNIOR should be a valid Experience enum value.");
         assertTrue(containsEnumValue("MID"), "MID should be a valid Experience enum value.");
         assertTrue(containsEnumValue("SENIOR"), "SENIOR should be a valid Experience enum value.");
@@ -108,9 +110,9 @@ class MainTest {
         assertTrue(seniorDeveloper instanceof Developer, "SeniorDeveloper should extend Developer.");
         assertEquals(Experience.SENIOR, seniorDeveloper.getExperience(), "Experience should be SENIOR.");
     }
-    
-    
-    
+
+
+
     /*-------------------DeveloperTaxTest-------------------*/
 
 
@@ -119,21 +121,21 @@ class MainTest {
     @Test
     @DisplayName("Test Get Simple Tax Rate")
     void testGetSimpleTaxRate() {
-        Double expectedSimpleTaxRate = 15d; 
+        Double expectedSimpleTaxRate = 15d;
         assertEquals(expectedSimpleTaxRate, developerTax.getSimpleTaxRate(), "The simple tax rate should be correct.");
     }
 
     @Test
     @DisplayName("Test Get Middle Tax Rate")
     void testGetMiddleTaxRate() {
-        Double expectedMiddleTaxRate = 25d; 
+        Double expectedMiddleTaxRate = 25d;
         assertEquals(expectedMiddleTaxRate, developerTax.getMiddleTaxRate(), "The middle tax rate should be correct.");
     }
 
     @Test
     @DisplayName("Test Get Upper Tax Rate")
     void testGetUpperTaxRate() {
-        Double expectedUpperTaxRate = 35d; 
+        Double expectedUpperTaxRate = 35d;
         assertEquals(expectedUpperTaxRate, developerTax.getUpperTaxRate(), "The upper tax rate should be correct.");
     }
 
